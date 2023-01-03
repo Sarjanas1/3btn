a59dfa94e33f888b7d719f2e5a3e3af775fa2029
BionXP <112658830+BionXP@users.noreply.github.com>
2023-01-03 11:11:09 +0800

    Update fileupdate.py

--- plugins/fileupdate.py
+++ plugins/fileupdate.py
@@ -14,7 +14,7 @@ from pyrogram.types import Message
 
 from config import ADMINS, LOGGER
 
-UPSTREAM_REPO = "https://github.com/ramadhani892/3-BUTTONS"
+UPSTREAM_REPO = "https://github.com/BionXP/3-BUTTONS"
 
 
 def gen_chlog(repo, diff):
